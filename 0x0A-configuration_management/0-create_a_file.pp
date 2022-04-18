e1c0d4aaab07529a69f7ff5bbac6adc368a8355c
sahinmeric <sahin.meric86@gmail.com>
2022-04-18 17:24:51 -0300

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file {'/tmp/school':
-path    => /tmp/school,
+path    => '/tmp/school',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
-content => 'I love puppet'
+content => 'I love Puppet'
 }
