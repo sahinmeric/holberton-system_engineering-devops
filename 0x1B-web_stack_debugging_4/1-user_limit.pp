192e5a7f5355f0a979ff520d82d91b6b29d9a846
sahinmeric <sahin.meric86@gmail.com>
2022-06-11 13:28:36 -0300

    user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Change user limits
+
+exec { 'Modify hard limit':
+    command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile unlimited/g" /etc/security/limits.conf',
+    provider => 'shell'
+}
+
+exec { 'Change soft limit':
+    command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 3000/g" /etc/security/limits.conf',
+    provider => 'shell'
+}
